f6aadce4489884525717ba5adaf56175c9b8393e
ChaviChauhan <157041069+ChaviChauhan@users.noreply.github.com>
2024-01-20 15:33:06 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27182,7 +27182,7 @@ var _bannerJpgDefault = parcelHelpers.interopDefault(_bannerJpg);
 const imageAltText = "purple and blue abstract background";
 /**
  * Sort description that expands on your title on the Home component.
- */ const description = "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+ */ const description = "I'm a Web Developer. I enjoy creating amazing UI in creative ways.";
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
